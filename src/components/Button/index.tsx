174c1b1ac0d81865f7461feb3a156832480f6afa
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-10-08 07:54:08 -0300

    App GoBarber

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Button } from './styles';
+
+const Button: React.FC = () => (
+  <Button></Button>
+);
+
+export default Button;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
+import { Container, Title } from './styles';
+
+// eslint-disable-next-line arrow-body-style
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+
+      <Input />
+      <Input />
+
+      <Button>Entrar</Button>
     </Container>
   );
 };
