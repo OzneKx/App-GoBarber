380d38f69f49fe0a508ec1a50c7a5356d03446c0
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-10-30 00:00:47 -0300

    AppGoBarber - GoStack

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -20,14 +20,17 @@ interface SignInCredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
+// eslint-disable-next-line react/prop-types
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -39,6 +42,8 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -67,7 +72,10 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{
+      user: data.user, loading, signIn, signOut,
+    }}
+    >
       {children}
     </AuthContext.Provider>
   );
